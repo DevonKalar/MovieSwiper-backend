fa6a67a34419c1d992ace86a0ef64dce957775a2
DevonKalar <devonkalar@gmail.com>
2025-09-14 18:23:09 -0700

    hotfix cors paths

--- src/config/env.ts
+++ src/config/env.ts
@@ -8,5 +8,5 @@ export const config = {
         openai: process.env.OPENAI_API_KEY!,
         tmdb: process.env.TMDB_API_KEY!,
     },
-    corsOrigins: process.env.CORS_ORIGINS?.split(',') || ['https://movieswiper.devonkalar.com', 'http://localhost:5173'],
+    corsOrigins: process.env.CORS_ORIGINS?.split(',') || ['https://movieswiper.devonkalar.com', 'http://localhost:5173', 'https://movie-swiper-devonkalars-projects.vercel.app', 'https://movie-swiper-git-master-devonkalars-projects.vercel.app' , 'https://movie-swiper-j70b24j6k-devonkalars-projects.vercel.app' ],
 };
